6d46803008e4fefa371d28922041d857fcaf862b
B3SCH <70465230+B3SCH@users.noreply.github.com>
2020-10-21 05:09:04 -0400

    Add files via upload

--- resizing_photo.py
+++ resizing_photo.py
@@ -10,12 +10,21 @@ def resizeImage(image, MAX_SIZE):
     PIL.Image --> PIL.Image
     """
     def aspectRatio(tuple):
+        """
+        tuple --> float
+        """
         return (tuple[0]/tuple[1])
 
     def resizeRatio(int):
+        """
+        int --> float
+        """
         return (MAX_SIZE[int]/image.size[int])
 
     def optimalImageSize(r):
+        """
+        float --> tuple
+        """
         optWidth = int(r * image.size[0])
         optHeight = int(r * image.size[1])
         return (optWidth, optHeight)
@@ -28,19 +37,44 @@ def resizeImage(image, MAX_SIZE):
     optSize = optimalImageSize(r)
     resizedImage = image.resize(size=optSize)
 
+    resizedImage.filename = image.filename
+
     return resizedImage
+
+def saveImageToDirectory(image, directory):
+    """
+    (PIL.Image, str) --> None
+    """
+    def checkDir(directory):
+        return os.path.isdir(directory)
+
+    def writeFileName(fileName):
+        splitName = fileName.split('/')
+        outfileName = 'POST_' + splitName[1]
+        return outfileName
+
+    if checkDir(directory) == False:
+        os.mkdir(directory)
+
+    outfileName = directory + writeFileName(image.filename)
+    image.save(outfileName)
+
+    return
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Rescale images")
 
-    parser.add_argument('-d', '--directory', type=str,
-                        required=False, help='Directory containing the images')
+    parser.add_argument('-d', '--in_directory', type=str, required=True,
+                            help='Directory containing preprocessed images')
 
-    parser.add_argument('-i', '--image', type=str,
-                        required=False, help='Name of image file')
+    parser.add_argument('-o', '--out_directory', type=str, required=True,
+                        help='Directory containing postprocessed images')
 
-    # THIS MAY NEED WORK -- add as a default?
+    parser.add_argument('-i', '--image', type=str, required=False,
+                        help='Name of image file')
+
+    # THIS MAY NEED WORK -- add default values?
     parser.add_argument('-s', '--size', type=int, nargs=2,
                         required=True, metavar=('width', 'height'),
                         help='Image size, tuple: (width, height)')
@@ -48,25 +82,25 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # define arguments
-    IMAGE = args.image
-    print("args.image type: ", type(IMAGE))
-    print("args.image value: ", IMAGE)
     MAX_SIZE = args.size
-    print("args.size type: ", type(MAX_SIZE))
-    print("args.image value: ", MAX_SIZE)
+    IN_DIRECTORY = args.in_directory
+    OUT_DIRECTORY = args.out_directory
 
-    # HERE ON DOWN, VARIABLE NAMES WILL NEED TO BE REWRITTEN
-    # this should happen within a for loop for images in dictionary
-    im = Image.open(IMAGE)
+    for img in os.listdir(IN_DIRECTORY):
+        im = Image.open(IN_DIRECTORY + img)
 
-    print("Image name: {}".format(im.filename))
-    print("Image dimensions: {}".format(im.size))
+        print("Image name: {}".format(im.filename))
+        print("Image dimensions: {}".format(im.size))
 
-    if (im.size[0] <= MAX_SIZE[0]) and (im.size[1] <= MAX_SIZE[1]):
-        print("Image dimensions are fine")
+        if (im.size[0] <= MAX_SIZE[0]) and (im.size[1] <= MAX_SIZE[1]):
+            print("Image dimensions are fine")
+            saveImageToDirectory(im, OUT_DIRECTORY)
 
-    if (im.size[0] > MAX_SIZE[0]) or (im.size[1] > MAX_SIZE[1]):
-        print("Image dimensions will need to be resized")
-        print("Entering resizing function...")
-        resizedImage = resizeImage(im, MAX_SIZE)
-        print("Resized image dimensions: {}".format(resizedImage.size))
+        if (im.size[0] > MAX_SIZE[0]) or (im.size[1] > MAX_SIZE[1]):
+            #print("Image dimensions will need to be resized")
+            #print("Entering resizing function...")
+            imResized = resizeImage(im, MAX_SIZE)
+            print("New image name: {}".format(imResized.filename))
+            print("New image dimensions: {}".format(imResized.size))
+            #print("Resized image dimensions: {}".format(resizedImage.size))
+            saveImageToDirectory(imResized, OUT_DIRECTORY)
